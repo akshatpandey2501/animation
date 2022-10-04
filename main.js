f0ce132e55be532dc774e669ae369f34607f6a2f
Akshat <akshatpandey2501@gmail.com>
2022-10-04 12:54:00 +0530

    fixed bugs related to energyball

--- main.js
+++ main.js
@@ -25,12 +25,27 @@ function changeimage4(){
     if(showimage.src.match('img/energyball1.png')){
         showimage.src='img/energyball2.png'
     }
+    else if(showimage.src.match('img/energyball3.png')){
+      showimage.src='img/energyball2.png'
+  }
 }
 function changeimage5(){
     let showimage=document.getElementById('energyball1')
     if(showimage.src.match('img/energyball1.png')){
         showimage.src='img/energyball3.png'
     }
+    else if(showimage.src.match('img/energyball2.png')){
+      showimage.src='img/energyball3.png'
+  }
+}
+function changeimage6(){
+  let showimage=document.getElementById('energyball1')
+  if(showimage.src.match('img/energyball2.png')){
+      showimage.src='img/energyball1.png'
+  }
+  else if(showimage.src.match('img/energyball3.png')){
+    showimage.src='img/energyball1.png'
+}
 }
 
 let m=-19;
@@ -64,7 +79,8 @@ const energysound2=new Audio("music/oodama_rasengan.mp3");
 window.addEventListener("keydown", (event) => {
     switch (event.key) {
       case "ArrowLeft":
-        changeimage2() 
+        changeimage2()
+        changeimage6() 
       ballmovementshow();
         window.setTimeout(ballmovement,1000);
      
